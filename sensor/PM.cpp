97aa20504973b1a4d4d72473bd3482e97f7b02e2
codyarvonen <codyarvonen@gmail.com>
2022-03-03 15:30:57 -0700

    Implement new PM sensor code, state machine, and rtc interrupts

--- sensor/PM.cpp
+++ sensor/PM.cpp
@@ -1,5 +1,5 @@
 // GEO Cookstove Sensor Code
-// C.O.N.D.O.R.S. 2020-2021
+// Los Encendidos 2021-2022
 // Arduino code for the cookstove sensor.
 // PM.cpp
 // This file contains function declarations for the PM sensor.
@@ -9,221 +9,185 @@
 #include "PM.h"
 
 void PM::measure() {
-  my_status = start_measurement();
-  my_status = read_measurement(); 
+//  my_status = start_measurement(); 
+  my_status = read_measurement();  // The read_measurement function stops and then starts the sensor measurement 
 
-  //Convert floats to strings in order to print to SD card
-  String stringPM01 = String(f_PM01);
-  String stringPM25 = String(f_PM25);
-  String stringPM4 = String(f_PM4);
-  String stringPM10 = String(f_PM10);
+  // Convert floats to strings in order to print to SD card
+  String stringPM2_5 = String(f_PN2_5);
+  String stringPM10 = String(f_PN10);
 
-  //TODO: Print statements commented; can bu uncommented for testing purposes
+  // Print statements commented; can be uncommented for testing purposes
   /*Serial.print("Read measurement status is ");
-  Serial.println(my_status, BIN);
-  Serial.print("PM1.0 value is ");
-  Serial.println(f_PM01);
-  Serial.print("PM2.5 value is ");
-  Serial.println(f_PM25);
-  Serial.print("PM4.0 value is ");
-  Serial.println(f_PM4);
-  Serial.print("PM10 value is ");
-  Serial.println(f_PM10);
-  Serial.println(" ");*/
+    Serial.print("PM1.0 GRIMM value is ");
+    Serial.println(f_PM1_0G);
+    Serial.print("PM2.5 GRIMM value is ");
+    Serial.println(f_PM2_5G);
+    Serial.print("PM10 GRIMM value is ");
+    Serial.println(f_PM10G);
+    Serial.print("PM1.0 TSI value is ");
+    Serial.println(f_PM1_0T);
+    Serial.print("PM2.5 TSI value is ");
+    Serial.println(f_PM2_5T);
+    Serial.print("PM10 TSI value is ");
+    Serial.println(f_PM10T);
+    Serial.print("Particles number >0.3um is ");
+    Serial.println(f_PN0_3);
+    Serial.print("Particles number >0.5um is ");
+    Serial.println(f_PN0_5);
+    Serial.print("Particles number >1.0um is ");
+    Serial.println(f_PN1_0);
+    Serial.print("Particles number >2.5um is ");
+    Serial.println(f_PN2_5);
+    Serial.print("Particles number >5.0um is ");
+    Serial.println(f_PN5_0);
+    Serial.print("Particles number >10um is ");
+    Serial.println(f_PN10);
+    Serial.println(" ");*/
 
   //Currently only PM2.5 and PM10 are printed to the .csv file, the other values can be returned if desired
-  pm25 = stringPM25;
+  pm2_5 = stringPM2_5;
   pm10 = stringPM10;
   
-  my_status = stop_measurement();
+//  my_status = stop_measurement();
 }
 
 bool PM::start_measurement(void)
 {
   // First, we send the command
-  byte start_measurement[] = {0x68, 0x01, 0x01, 0x96 };
+  byte start_measurement[] = {0x11, 0x03, 0x0C, 0x02, 0x1E, 0xC0};
   Serial3.write(start_measurement, sizeof(start_measurement));
-  //Then we wait for the response
-  while(Serial3.available() < 2);
-  byte read1 = Serial3.read();
-  byte read2 = Serial3.read();
+  // Then we wait for the responce
+  while(Serial3.available() < 1);
+  byte HEAD = Serial3.read();
+  while(Serial3.available() < 1);
+  byte LEN = Serial3.read();
+  while(Serial3.available() < 1);
+  byte CMD = Serial3.read();
+  while(Serial3.available() < 1);
+  byte DF1 = Serial3.read();
+  while(Serial3.available() < 1);
+  byte CS = Serial3.read();
   // Test the response
-  if ((read1 == 0xA5) && (read2 == 0xA5)){
-    // ACK
-    return 1;
+  if ((0x100 - HEAD - LEN - CMD - DF1) != CS) { 
+    Serial.println("Start measurement checksum fail");
+    return false;
   }
-  else if ((read1 == 0x96) && (read2 == 0x96))
-  {
-    // NACK
-    return 0;
+  else {
+    Serial.println("Start measurement checksum success!");
+    return true;
   }
-  else return 0;
 }
 
 bool PM::stop_measurement(void)
 {
   // First, we send the command
-  byte stop_measurement[] = {0x68, 0x01, 0x02, 0x95 };
+  byte stop_measurement[] = {0x11, 0x03, 0x0C, 0x01, 0x1E, 0xC1};
   Serial3.write(stop_measurement, sizeof(stop_measurement));
-  //Then we wait for the response
-  while(Serial3.available() < 2);
-  byte read1 = Serial3.read();
-  byte read2 = Serial3.read();
+  // Then we wait for the responce
+  while(Serial3.available() < 1);
+  byte HEAD = Serial3.read();
+  while(Serial3.available() < 1);
+  byte LEN = Serial3.read();
+  while(Serial3.available() < 1);
+  byte CMD = Serial3.read();
+  while(Serial3.available() < 1);
+  byte DF1 = Serial3.read();
+  while(Serial3.available() < 1);
+  byte CS = Serial3.read();
   // Test the response
-  if ((read1 == 0xA5) && (read2 == 0xA5)){
-    // ACK
-    return 1;
+  if ((0x100 - HEAD - LEN - CMD - DF1) != CS) { 
+    Serial.println("Stop measurement checksum fail");
+    return false;
   }
-  else if ((read1 == 0x96) && (read2 == 0x96))
-  {
-    // NACK
-    return 0;
+  else {
+    return true;
   }
-  else return 0;
 }
 
 bool PM::read_measurement (void)
 {
-  // Send the command 0x68 0x01 0x04 0x93
-  byte read_particle[] = {0x68, 0x01, 0x04, 0x93 };
-  Serial3.write(read_particle, sizeof(read_particle));
-  // A measurement can return 0X9696 for NACK
-  // Or can return eight bytes if successful
-  // We wait for the first two bytes
+  // First, we send the command
+  byte read_measurement[] = {0x11, 0x02, 0x0B, 0x07, 0xDB};
+  Serial3.write(read_measurement, sizeof(read_measurement));
+  // Then we wait for the responce
   while(Serial3.available() < 1);
-  delay(1000); 
   byte HEAD = Serial3.read();
-  //Serial.println(HEAD, BIN);
-  //Serial.println(0x40, BIN);
   while(Serial3.available() < 1);
-  delay(1000);
   byte LEN = Serial3.read();
-  //Serial.println(LEN, BIN);
-  //Serial.println(0xd, BIN); //was 0x05 but now 0xd
-  // Test the response
-  if ((HEAD == 0x96) && (LEN == 0x96)){
-    // NACK
-    Serial.println("NACK");
-    return 0;
+  while(Serial3.available() < 1);
+  byte CMD = Serial3.read();
+  byte DF[52];
+  for (int i = 0; i < 52; i++) { // Iterate through the input until all data bytes are collected
+    while(Serial3.available() < 1);
+    DF[i] = Serial3.read();
   }
-  else if ((HEAD == 0x40) && (LEN == 0xd))//was 0x05 but now 0xd
-  {
-    // The measurement is valid, read the rest of the data 
-    // wait for the next byte
-    // TODO: test print statements commented out; can un-comment for testing purposes 
-    while(Serial3.available() < 1);
-    byte COMD = Serial3.read();
-    //Serial.println(COMD, HEX);
-    
-    while(Serial3.available() < 1);
-    byte DF1 = Serial3.read();
-    //Serial.println(DF1, HEX);
-    
-    while(Serial3.available() < 1);
-    byte DF2 = Serial3.read();
-    //Serial.println(DF2, HEX);     
-    
-    while(Serial3.available() < 1);
-    byte DF3 = Serial3.read();
-    //Serial.println(DF3, HEX); 
-      
-    while(Serial3.available() < 1);
-    byte DF4 = Serial3.read();
-    //Serial.println(DF4, HEX);         
-    while(Serial3.available() < 1);
-    byte DF5 = Serial3.read();
-    //Serial.println(DF5, HEX);
-    while(Serial3.available() < 1);
-    byte DF6 = Serial3.read();
-    //Serial.println(DF6, HEX);     
-    while(Serial3.available() < 1);
-    byte DF7 = Serial3.read();
-    //Serial.println(DF7, HEX);   
-    while(Serial3.available() < 1);
-    byte DF8 = Serial3.read();
-    //Serial.println(DF8, HEX);     
-    while(Serial3.available() < 1);
-    byte DF9 = Serial3.read();
-    //Serial.println(DF9, HEX);
-    while(Serial3.available() < 1);
-    byte DF10 = Serial3.read();
-    //Serial.println(DF10, HEX);     
-    while(Serial3.available() < 1);
-    byte DF11 = Serial3.read();
-    //Serial.println(DF11, HEX);   
-    while(Serial3.available() < 1);
-    byte DF12 = Serial3.read();
-    //Serial.println(DF12, HEX);     
-    while(Serial3.available() < 1);
-    byte CS = Serial3.read();
-    //Serial.println(CS, HEX);
+  while(Serial3.available() < 1);
+  byte CS = Serial3.read();
+  // Then we add all the data bytes together and store it as an int
+  int DATA = 0;
+  for (int i = 0; i < 52; i++) {
+    DATA += DF[i];
+  }
+
+  int response_sum = HEAD + LEN + CMD + DATA;
+  int factor = (response_sum/256) + 1;
+
+  // Uncomment for debbugging
+  /*Serial.println("HEAD = ");
+  Serial.println(HEAD);
+  Serial.println("LEN = ");
+  Serial.println(LEN);
+  Serial.println("CMD = ");
+  Serial.println(CMD);
+  Serial.println("DATA = ");
+  Serial.println(DATA);
+  Serial.println("CS = ");
+  Serial.println(CS);
+  Serial.println("Response Sum = ");
+  Serial.println(response_sum);
+  Serial.println("Factor = ");
+  Serial.println(factor);
+  Serial.println("((0x100 * factor) - response_sum) = ");
+  Serial.println(((0x100 * factor) - response_sum));*/
   
-       
-    // Now we shall verify the checksum
-    if (((0x10000 - HEAD - LEN - COMD - DF1 - DF2 - DF3 - DF4 - DF5 - DF6 - DF7 - DF8 - DF9 - DF10 - DF11 - DF12) % 0x100) != CS){ 
-      Serial.println("Checksum fail");
-      return 0;
-    }
-    else
-    {
-      // Checksum OK, we compute PM values
-      byte PM01 = (DF1 * 0x100) + DF2;
-      byte PM25 = (DF3 * 0x100) + DF4;
-      byte PM4 = (DF5 * 0x100) + DF6;
-      byte PM10 = (DF7 * 0x100) + DF8;  
-      f_PM01 = float(PM01);
-      f_PM25 = float(PM25);
-      f_PM4 = float(PM4);
-      f_PM10 = float(PM10);
-      return 1;
-    }
+  // Test the response
+  if (((0x100 * factor) - response_sum) != CS) { 
+    Serial.println("Read measurement checksum FAIL");
+    reset_measurement();
+    return false;
+  }
+  else {
+    Serial.println("Computing values");
+    // Compute PM values
+    f_PM1_0G = DF[0]*0x1000000 + DF[1]*0x10000 + DF[2]*0x100 + DF[3];
+    f_PM2_5G = DF[4]*0x1000000 + DF[5]*0x10000 + DF[6]*0x100 + DF[7];
+    f_PM10G = DF[8]*0x1000000 + DF[9]*0x10000 + DF[10]*0x100 + DF[11];
+    f_PM1_0T = DF[12]*0x1000000 + DF[13]*0x10000 + DF[14]*0x100 + DF[15];
+    f_PM2_5T = DF[16]*0x1000000 + DF[17]*0x10000 + DF[18]*0x100 + DF[19];
+    f_PM10T = DF[20]*0x1000000 + DF[21]*0x10000 + DF[22]*0x100 + DF[23];
+    f_PN0_3 = DF[24]*0x1000000 + DF[25]*0x10000 + DF[26]*0x100 + DF[27];
+    f_PN0_5 = DF[28]*0x1000000 + DF[29]*0x10000 + DF[30]*0x100 + DF[31];
+    f_PN1_0 = DF[32]*0x1000000 + DF[33]*0x10000 + DF[34]*0x100 + DF[35];
+    f_PN2_5 = DF[36]*0x1000000 + DF[37]*0x10000 + DF[38]*0x100 + DF[39];
+    f_PN5_0 = DF[40]*0x1000000 + DF[41]*0x10000 + DF[42]*0x100 + DF[43];
+    f_PN10 = DF[44]*0x1000000 + DF[45]*0x10000 + DF[46]*0x100 + DF[47];
+    
+    return true;
   }
 }
 
-bool PM::stop_autosend(void)
-{
- // Stop auto send
-  byte stop_autosend[] = {0x68, 0x01, 0x20, 0x77};
-  Serial3.write(stop_autosend, sizeof(stop_autosend));
-  //Then we wait for the response
-  Serial.print("stop autosend sent \n");
-  while(Serial3.available() < 2);
-  byte read1 = Serial3.read();
-  byte read2 = Serial3.read();
-  // Test the response
-  if ((read1 == 0xA5) && (read2 == 0xA5)){
-    // ACK
-    Serial.print(read1, HEX);
-    Serial.print(read2, HEX);
-    return 1;
-  }
-  else if ((read1 == 0x96) && (read2 == 0x96))
-  {
-    // NACK
-    Serial.print(read1,HEX);
-    Serial.print(read2,HEX);    return 0;
-  }
-  else{Serial.print(read1, HEX);Serial.print(read2, HEX);; return 0;}
-}
+void PM::reset_measurement(void) {
+//  Serial.println("RESET MEASUREMENT");
+//  Serial.println("Closing and opening measurment ");
+//  delay(1000);
+  
+  my_status = stop_measurement();
+//  Serial.println("Stop measurement status is ");
+//  Serial.println(my_status);
+  delay(1000);
 
-bool PM::start_autosend(void)
-{
- // Start auto send
-  byte start_autosend[] = {0x68, 0x01, 0x40, 0x57 };
-  Serial3.write(start_autosend, sizeof(start_autosend));
-  //Then we wait for the response
-  while(Serial3.available() < 2);
-  byte read1 = Serial3.read();
-  byte read2 = Serial3.read();
-  // Test the response
-  if ((read1 == 0xA5) && (read2 == 0xA5)){
-    // ACK
-    return 1;
-  }
-  else if ((read1 == 0x96) && (read2 == 0x96))
-  {
-    // NACK
-    return 0;
-  }
-  else return 0;
+  my_status = start_measurement();
+//  Serial.println("Start measurement status is ");
+//  Serial.println(my_status);
+  delay(1000);
 }
--- sensor/sensor.ino
+++ sensor/sensor.ino
@@ -1,24 +1,27 @@
 // GEO Cookstove Sensor Code
-// C.O.N.D.O.R.S. 2020-2021
+// Los Encendidos 2021-2022
 // Arduino code for the cookstove sensor
 // sensor.ino
 // This file contains setup() and loop(), as well as functions for the LED and SD card
 
-
-#include <SD.h>   // Used to read/write files to the SD card
-#include <SPI.h>  // Used to communicate with the SD card reader
+#include <SD.h>       // Used to read/write files to the SD card
+#include <SPI.h>      // Used to communicate with the SD card reader
 #include <RTClib.h>   // Library for the real time clock
 #include <String.h>   // Required to use string data type
-#include "PM.h"   // Header file with PM functions/data
-#include "CO2.h"  // Header file with CO2 functions/data
-#include "CO.h"   // Header file with CO functions/data
+#include "PM.h"       // Header file with PM functions/data
+#include "CO2.h"      // Header file with CO2 functions/data
+#include "CO.h"       // Header file with CO functions/data
+
+#include <LowPower.h> // Library to enable sleep mode
 
 // PIN NUMBERS
-#define RED 24  // Pin number for Red in RGB LED
+#define RED 24    // Pin number for Red in RGB LED
 #define GREEN 26  // Pin number for Green in RGB LED
 #define BLUE 22   // Pin number for Blue in RGB LED
-#define POWER 13   // Pin number for MOSFET (controls power to the fan)
-#define CS 53   // Pin number for SD card reader
+#define FAN 13    // Pin number for MOSFET (controls power to the fan)
+#define CS 53     // Pin number for SD card reader
+
+#define INTERRUPT_PIN 18
 
 // OBJECTS
 RTC_PCF8523 rtc;  // Real time clock object
@@ -27,58 +30,184 @@ CO2 co2;
 CO co;
 
 // GLOBAL VARIABLES
-File myFile;  // File variable for writing to SD card
+File myFile;      // File variable for writing to SD card
 String fileName = "datos.csv";
 
+String pm2_5;
+String pm10;
+double ppmCO = 0;
+double ppmCO2 = 0;
+
+enum state {sleep, fanOn, fanOff, measure, writeData, errorSD, errorMeasure};
+enum state currentState;
+
 void setup() {
+
+  pinMode(INTERRUPT_PIN, INPUT_PULLUP); // Initialize the interrupt pin with the internal pull up resistor in the Arduino
+//  pinMode(LED_BUILTIN, OUTPUT);
+
+  pinMode(FAN, OUTPUT);       // Configures MOSFET pin as OUTPUT
+  pinMode(RED, OUTPUT);       // Configures RED pin as OUTPUT
+  pinMode(BLUE, OUTPUT);      // Configures BLUE pin as OUTPUT
+  pinMode(GREEN, OUTPUT);     // Configures GREEN pin as OUTPUT
+  digitalWrite(FAN, LOW);     // Sets MOSFET pin to LOW
+  LED_OFF();                  // Sets color pins to LOW
+  delay(500);
+  
+  LED_ON();
+  delay(2000);
   
   Serial.begin(9600);   // Begin serial communication for printing to serial monitor
   Serial3.begin(9600);  // Serial3 will be used to communicate with the PM sensor
-  Serial.println("CONDORS Cookstove Sensor");
+  Serial.println("Los Encendidos Cookstove Sensor");
 
-  while (!SD.begin()) {   // Initializes SD card, LED will flash until SD card is present
-    LED_RED();
-    delay(1000);
-    LED_OFF();
-    delay(1000);
-  }
-  
-  rtc.begin();  // Initializes real time clock, uses RTC library
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));   // Will set the real time clock to the time from the computer system
-  printHeader();  // Prints header to the csv file on the SD card
+  initSensors(true, true, true, true);
 
-  pm.my_status = pm.stop_autosend();  // Initializes PM sensor, returns status
-  co2.scd30.begin();  // Intializes CO2 sensor
-  co2.scd30.setMeasurementInterval(1);  //Fastest communication time with CO2 Sensor
+  // https://adafruit.github.io/RTClib/html/class_r_t_c___p_c_f8523.html
+  // https://brainwagon.org/2019/04/03/template-program-that-uses-a-ds3231-rtc-to-wake-up-an-arduino/
 
-  pinMode(POWER, OUTPUT);   // Configures MOSFET pin as OUTPUT
-  digitalWrite(POWER, LOW);   // Sets MOSFET pin to LOW
-  
-  LED_GREEN();
-  
+  currentState = fanOff;
+
+  SD.begin();
+
+  LED_OFF();
+  delay(2000);
 }
 
 void loop() {
 
-  Serial.println("MOSFET HIGH");
-  digitalWrite(POWER, HIGH);  // Turns on power to the fan
-  delay(3000);
-  
-  pm.measure();   // PM sensor reads measurements, variables in PM library will contain the results
-  String pm25 = pm.pm25;  // Concentration of PM that is 2.5micrometers
-  String pm10 = pm.pm10;  // Concentration of PM that is 10micrometers
-  double ppmCO = co.measure();  // Concentration of CO in parts per million
-  double ppmCO2 = co2.measure();  // Concentration of CO2 in parts per million
-  
-  DateTime now = rtc.now();   // The current time as returned by the real time clock
-  writeToFile(now, ppmCO, ppmCO2, pm25, pm10);
+  static int fanTimer = 0;
+  static bool writeSuccess = false;
+  static bool SDCardSuccess = false;
 
-  Serial.println("MOSFET LOW");
-  digitalWrite(POWER, LOW);   //Turns off power to the fan
-  delay(3000);
+  // Perform state update first.
+  switch(currentState) { 
+    case sleep:
+      digitalWrite(FAN, HIGH);
+      currentState = fanOn;
+      break;
+    case fanOn:
+      if (fanTimer == 5) {
+        fanTimer = 0;
+        digitalWrite(FAN, LOW);
+        currentState = fanOff;
+      }
+      else {
+        currentState = fanOn;
+      }
+      break;
+    case fanOff:
+      if (fanTimer == 5) {
+        fanTimer = 0;
+        currentState = measure;
+      }
+      else {
+        currentState = fanOff;
+      }
+      break;
+    case measure:
+      currentState = writeData;
+      break;
+    case writeData:
+      if (writeSuccess) {
+        currentState = sleep;
+      }
+      else {
+        currentState = errorSD;
+      }
+      break;
+    case errorSD:
+      if (SDCardSuccess && printHeader()) {
+        digitalWrite(FAN, HIGH);
+        currentState = fanOn;
+      }
+      else {
+        currentState = errorSD;
+      }
+      break;
+    case errorMeasure:
+      // IMPLEMENT THIS STATE (CHECKSUM FAIL, ETC.)
+      break;
+    default:
+      Serial.println("ERROR: No state has been specified");
+      break;
+  }
 
+  // Perform state action next.
+  switch(currentState) { 
+    case sleep:
+      Serial.println("STATE: sleep");
+      LED_OFF();
+
+      Serial.println("MCU going to sleep...");
+
+      rtc.enableCountdownTimer(PCF8523_FrequencyMinute, 1);
+
+
+      delay(2000);
+      attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), isr, FALLING);
+      LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
+      
+      detachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN));
+      rtc.disableCountdownTimer();
+      delay(2000);
+      
+      break;
+    case fanOn:
+      Serial.println("STATE: fanOn");
+      LED_GREEN();
+      fanTimer++;
+      break;
+    case fanOff:
+      Serial.println("STATE: fanOff");
+      LED_OFF();
+      fanTimer++;
+      break;
+    case measure:
+      Serial.println("STATE: measure");
+      LED_BLUE();
+      pm.measure();             // PM sensor reads measurements, variables in PM library will contain the results
+      pm2_5 = pm.pm2_5;         // Concentration of PM that is 2.5micrometers
+      pm10 = pm.pm10;           // Concentration of PM that is 10micrometers
+      ppmCO = co.measure();     // Concentration of CO in parts per million
+      ppmCO2 = co2.measure();   // Concentration of CO2 in parts per million
+      break;
+    case writeData:
+      Serial.println("STATE: writeData");
+      writeSuccess = writeToFile(rtc.now(), ppmCO, ppmCO2, pm2_5, pm10);
+      break;
+    case errorSD:
+      Serial.println("STATE: errorSD");
+      LED_RED();
+      SDCardSuccess = SD.begin();
+      break;
+    case errorMeasure:
+      // IMPLEMENT THIS STATE (CHECKSUM FAIL, ETC.)
+      Serial.println("STATE: errorMeasure");
+      break;
+    default:
+      Serial.println("ERROR: No state has been specified");
+      LED_OFF();
+      break;
+  }
+  
+  delay(1000);
+  
 }
 
+/*
+ * Interrupt service routine
+ */
+ 
+void isr() {
+  Serial.println("MCU is now awake");
+}
+
+
+/*
+ * Helper functions
+ */
+ 
 void LED_ON() {
     digitalWrite(RED, HIGH);
     digitalWrite(GREEN, HIGH);
@@ -117,11 +246,35 @@ void LED_BLUE() {
     return;
 }
 
-void printHeader() { //Prints column headers to csv file
+void LED_YELLOW() {
+    //Turns LED Yellow
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(BLUE, LOW);
+    return;
+}
+
+void LED_CYAN() {
+    //Turns LED Cyan
+    digitalWrite(RED, LOW);
+    digitalWrite(GREEN, HIGH);
+    digitalWrite(BLUE, HIGH);
+    return;
+}
+
+void LED_MAGENTA() {
+    //Turns LED Magenta
+    digitalWrite(RED, HIGH);
+    digitalWrite(GREEN, LOW);
+    digitalWrite(BLUE, HIGH);
+    return;
+}
+
+bool printHeader() { //Prints column headers to csv file
     myFile = SD.open(fileName, FILE_WRITE);
     
     if (myFile) { // Write to the file if it opened correctly
-        Serial.println("Successfully connected to SD card");
+        Serial.println("Successfully opened file on SD card!");
         //Prints the headers to the SD card file
         myFile.print("Fecha");  //Date
         myFile.print(",      ");
@@ -137,21 +290,21 @@ void printHeader() { //Prints column headers to csv file
         myFile.print(",      ");
         myFile.println("      ");
         myFile.close();
-        return;
+        Serial.println("Successfully printed header to file on SD card!");
+        return true;
     }
     else {
-        Serial.println("Failed to connect to SD card");
-        LED_RED();
-        delay(5000);
-        return;
+        Serial.println("Failed to open file on SD card");
+        return false;
     }
 }
 
-void writeToFile(DateTime now, double CO, double CO2, String pm25, String pm10) { //Writes most recent values to the csv file
+// Writes most recent values to the csv file
+bool writeToFile(DateTime now, double CO, double CO2, String pm2_5, String pm10) {
     myFile = SD.open(fileName, FILE_WRITE);
     
     if (myFile) {  // Write to the file if it opened correctly
-        Serial.println("Successfully connected to SD card");
+        Serial.println("Successfully opened file on SD card!");
 
         //Write Date Day/Month/Year (Custom in Peru)
         myFile.print(now.day(), DEC);
@@ -174,19 +327,36 @@ void writeToFile(DateTime now, double CO, double CO2, String pm25, String pm10) 
         myFile.print(",      ");
         myFile.print(CO2);    //CO2 concentration, ppm
         myFile.print(",      ");
-        myFile.print(pm25);    //PM 2.5um concentration,
+        myFile.print(pm2_5);    //PM 2.5um concentration,
         myFile.print(",      ");
         myFile.print(pm10);    //PM 10um concentration,
         myFile.print(",      ");
         myFile.println("      ");
         myFile.close();
-        return;
+        
+        Serial.println("Successfully written data to file on SD card!");
+        
+        return true;
     }
     else {
-        Serial.println("Failed to connect to SD card");
-        LED_RED();
-        delay(5000);
-        return;
+        Serial.println("Failed to open file on SD card");
+        return false;
     }
+}
 
+void initSensors(bool pmInit, bool coInit, bool co2Init, bool rtcInit) {
+  if (pmInit) {
+    pm.reset_measurement();
+  }
+  if (co2Init) {
+    co2.scd30.begin();  // Intializes CO2 sensor
+    co2.scd30.setMeasurementInterval(1);  // Fastest communication time with CO2 Sensor
+  }
+  if (coInit) {
+    
+  }
+  if (rtcInit) {
+    rtc.begin();  // Initializes real time clock, uses RTC library
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));   // Will set the real time clock to the time from the computer system
+  }
 }
